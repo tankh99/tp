6e7a53eed9f0d8d7709dacfbed7822bb9cd7782d
tankh99 <khanghou99@gmail.com>
2024-02-21 15:54:59 +0800

    Revert "Add remark command for edit and remark"
    This reverts commit f0c23f8e85139791e0be831d4e934a37c8fab735.

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -1,23 +1,21 @@
 package seedu.address.model.person;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
-import static seedu.address.logic.parser.CliSyntax.*;
 
-import java.util.*;
-import java.util.logging.Logger;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
-import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.util.ToStringBuilder;
-import seedu.address.logic.parser.Prefix;
 import seedu.address.model.tag.Tag;
-import seedu.address.storage.JsonAddressBookStorage;
 
 /**
  * Represents a Person in the address book.
  * Guarantees: details are present and not null, field values are validated, immutable.
  */
 public class Person {
-    private static final Logger logger = LogsCenter.getLogger(Person.class);
+
     // Identity fields
     private final Name name;
     private final Phone phone;
@@ -26,19 +24,17 @@ public class Person {
     // Data fields
     private final Address address;
     private final Set<Tag> tags = new HashSet<>();
-    private final Remark remark;
 
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags, Remark remark) {
+    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
         requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
         this.tags.addAll(tags);
-        this.remark = remark;
     }
 
     public Name getName() {
@@ -65,9 +61,6 @@ public class Person {
         return Collections.unmodifiableSet(tags);
     }
 
-    public Remark getRemark() {
-        return remark;
-    }
     /**
      * Returns true if both persons have the same name.
      * This defines a weaker notion of equality between two persons.
@@ -101,14 +94,13 @@ public class Person {
                 && phone.equals(otherPerson.phone)
                 && email.equals(otherPerson.email)
                 && address.equals(otherPerson.address)
-                && remark.equals(otherPerson.remark)
                 && tags.equals(otherPerson.tags);
     }
 
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, remark, tags);
+        return Objects.hash(name, phone, email, address, tags);
     }
 
     @Override
@@ -118,42 +110,8 @@ public class Person {
                 .add("phone", phone)
                 .add("email", email)
                 .add("address", address)
-                .add("remark", remark)
                 .add("tags", tags)
                 .toString();
     }
 
-    /**
-     * Creates a new instance of the current person object and updates only specified values in the new
-     * @return
-     */
-    public Person toNewPerson(Map<Prefix, Object> editedValues) {
-        Name name = this.name;
-        Phone phone = this.phone;
-        Email email = this.email;
-        Address address = this.address;
-        Set<Tag> tags = this.tags;
-        Remark remark = this.remark;
-        for (Map.Entry<Prefix, Object> entry: editedValues.entrySet()) {
-            Object value = entry.getValue();
-            Prefix key = entry.getKey();
-            if (key == PREFIX_NAME) {
-                name = (Name) value;
-            } else if (key == PREFIX_PHONE) {
-                phone = (Phone) value;
-            } else if (key == PREFIX_EMAIL) {
-                email = (Email) value;
-            } else if (key == PREFIX_ADDRESS) {
-                address = (Address) value;
-            } else if (key == PREFIX_TAG) {
-                tags = new HashSet<Tag>();
-                tags.add((Tag) value);
-            } else if (key == PREFIX_REMARK) {
-                remark = (Remark) value;
-            } else {
-                logger.severe("Received an unknown prefix " + key + " with value " + value);
-            }
-        }
-        return new Person(name, phone, email, address, tags, remark);
-    }
 }
