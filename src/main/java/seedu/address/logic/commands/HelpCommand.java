596ed018e6dec5989a2bc535a16868aed1fe5e6f
Jerome-j <49815108+Jerome-j@users.noreply.github.com>
2024-03-30 21:25:20 +0800

    Update help message

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import seedu.address.model.Model;
+import seedu.address.ui.HelpWindow;
 
 /**
  * Format full help instructions for every command for display.
@@ -12,7 +13,8 @@ public class HelpCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
             + "Example: " + COMMAND_WORD;
 
-    public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
+    public static final String SHOWING_HELP_MESSAGE = "For the complete user guide, please visit: " +
+            HelpWindow.USERGUIDE_URL;
 
     @Override
     public CommandResult execute(Model model) {
