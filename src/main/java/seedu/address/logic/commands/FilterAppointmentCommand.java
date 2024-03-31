144c1c54d5d2eee791b7746fcf9c1485f9f3f96e
vnnamng <e0969384@u.nus.edu>
2024-03-31 16:28:07 +0800

    Add FilterAppointmentCommand

---
+++ src/main/java/seedu/address/logic/commands/FilterAppointmentCommand.java
@@ -0,0 +1,63 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.parser.CliSyntax;
+import seedu.address.model.Model;
+import seedu.address.model.appointment.Appointment;
+
+import java.util.function.Predicate;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Filter the appointment in the CogniCare appointment list which happen on the specified datetimes.
+ */
+public class FilterAppointmentCommand extends Command {
+
+    public static final String COMMAND_WORD = "filterappointment";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Filter the appointment which happens " +
+            "on the specified date. Parameters: "
+            + CliSyntax.PREFIX_START_DATETIME + "START_DATETIME "
+            + CliSyntax.PREFIX_END_DATETIME + "END_DATETIME\n"
+            + "Example: " + COMMAND_WORD + " "
+            + CliSyntax.PREFIX_START_DATETIME + "2024-03-18 09:00"
+            + CliSyntax.PREFIX_END_DATETIME + "2024-03-18 18:00";
+
+    private final Predicate<Appointment> predicate;
+
+    public FilterAppointmentCommand(Predicate<Appointment> predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredAppointmentList(predicate);
+        return new CommandResult(String.format(Messages.MESSAGE_APPOINTMENTS_LISTED_OVERVIEW,
+                model.getFilteredAppointmentList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof FilterAppointmentCommand)) {
+            return false;
+        }
+
+        FilterAppointmentCommand otherAppointmentFindCommand = (FilterAppointmentCommand) other;
+        return predicate.equals(otherAppointmentFindCommand.predicate);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("predicate", predicate)
+                .toString();
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/FilterAppointmentCommandParser.java
@@ -0,0 +1,45 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.FilterAppointmentCommand;
+import seedu.address.logic.commands.FindAppointmentCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.appointment.EndDateTime;
+import seedu.address.model.appointment.FilterAppointmentPredicate;
+import seedu.address.model.appointment.FindAppointmentPredicate;
+import seedu.address.model.appointment.StartDateTime;
+
+import java.util.logging.Filter;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_APPOINTMENT_ID;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_END_DATETIME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PATIENT_ID;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_START_DATETIME;
+
+/**
+ * Parses input arguments and creates a new FindAppointmentCommand object
+ */
+public class FilterAppointmentCommandParser implements Parser<FilterAppointmentCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindAppointmentCommand
+     * and returns a FindAppointmentCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public FilterAppointmentCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_START_DATETIME, PREFIX_END_DATETIME);
+
+        if (!ParserUtil.arePrefixesPresent(argMultimap, PREFIX_START_DATETIME, PREFIX_END_DATETIME)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterAppointmentCommand.MESSAGE_USAGE));
+        }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_START_DATETIME, PREFIX_END_DATETIME);
+        StartDateTime startDateTime = ParserUtil.parseStartDateTime(argMultimap.getValue(PREFIX_START_DATETIME).get());
+        EndDateTime endDateTime = ParserUtil.parseEndDateTime(argMultimap.getValue(PREFIX_END_DATETIME).get());
+
+        return new FilterAppointmentCommand(new FilterAppointmentPredicate(startDateTime, endDateTime));
+    }
+
+}
