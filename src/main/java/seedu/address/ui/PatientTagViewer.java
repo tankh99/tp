fbc5b6d3df0a2c925ececbd7578166dea4757799
Jerome-j <49815108+Jerome-j@users.noreply.github.com>
2024-04-04 05:11:07 +0800

    Fix checkstyle

--- src/main/java/seedu/address/ui/PatientTagViewer.java
+++ src/main/java/seedu/address/ui/PatientTagViewer.java
@@ -1,42 +1,41 @@
 package seedu.address.ui;
 
-import javafx.fxml.FXML;
-
-
-import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.Region;
-import seedu.address.model.Model;
-import seedu.address.model.ReadOnlyPatientList;
-
 import java.util.Comparator;
 import java.util.HashMap;
 
-public class PatientTagViewer extends UiPart<Region> {
-    private static final String FXML = "PatientTagViewer.fxml";
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Region;
+import seedu.address.model.ReadOnlyPatientList;
 
+/**
+ * Represents a class that displays all patient tags in main UI.
+ */
+public class PatientTagViewer extends UiPart<Region> {
     public static final int MAX_TAGS_TO_BE_DISPLAYED = 20;
+
+    private static final String FXML = "PatientTagViewer.fxml";
 
     @FXML
     private FlowPane tags;
 
+    /**
+     * Renders the Patient alias as  Labels
+     * @param model which contains all patient information.
+     */
     public PatientTagViewer(ReadOnlyPatientList model) {
         super(FXML);
 
-        // Set horizontal and vertical gaps
-        tags.setHgap(10); // Set the horizontal gap between children
-        tags.setVgap(10); // Set the vertical gap between children
-
-
-
+        // Create as a HashMap to store the tagInformation as key and count as value
         HashMap<String, Integer> tagInfo = new HashMap<>();
 
+        // Iterate all person, and their tag to count how many occurrence.
         model.getPersonList().stream().forEach(person -> {
             person.getTags().stream().forEach(tag -> {
                 tagInfo.put(tag.tagName, tagInfo.getOrDefault(tag.tagName, 0) + 1);
             });
         });
-
 
 
         tagInfo.entrySet().stream()
@@ -45,7 +44,7 @@ public class PatientTagViewer extends UiPart<Region> {
                 .sorted(Comparator.comparingInt(entry -> -entry.getValue()))
                 // Limit length to prevent malicious users.
                 .limit(MAX_TAGS_TO_BE_DISPLAYED).forEach(tag -> {
-                    addStyledLabel(tag.getKey() + " (" + tag.getValue() + ")");
+                    addLabel(tag.getKey() + " (" + tag.getValue() + ")");
                 }
         );
     }
@@ -53,8 +52,7 @@ public class PatientTagViewer extends UiPart<Region> {
 
 
 
-    // Method to add styled label
-    private void addStyledLabel(String labelText) {
+    private void addLabel(String labelText) {
         Label label = new Label(labelText);
         tags.getChildren().add(label);
     }
