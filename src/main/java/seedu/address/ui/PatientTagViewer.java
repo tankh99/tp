285c7c4637e73e9eb27c72e9c7846f5b6e821ac5
Jerome-j <49815108+Jerome-j@users.noreply.github.com>
2024-04-04 05:26:19 +0800

    Auto update

--- src/main/java/seedu/address/ui/PatientTagViewer.java
+++ src/main/java/seedu/address/ui/PatientTagViewer.java
@@ -3,11 +3,14 @@ package seedu.address.ui;
 import java.util.Comparator;
 import java.util.HashMap;
 
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
 import seedu.address.model.ReadOnlyPatientList;
+import seedu.address.model.patient.Patient;
 
 /**
  * Represents a class that displays all patient tags in main UI.
@@ -20,19 +23,34 @@ public class PatientTagViewer extends UiPart<Region> {
     @FXML
     private FlowPane tags;
 
+    private final ObservableList<Patient> patientList;
+
     /**
      * Renders the Patient alias as  Labels
      * @param model which contains all patient information.
      */
     public PatientTagViewer(ReadOnlyPatientList model) {
         super(FXML);
+        patientList = model.getPersonList();
+
+        // Add listener to PersonList.
+        // Inspired by: https://stackoverflow.com/questions/23335522/
+        patientList.addListener((ListChangeListener.Change<? extends Patient> change) -> updateTags());
+
+        updateTags();
+    }
+
+    private void updateTags() {
+        // Clear existing tags
+        tags.getChildren().clear();
 
         // Create as a HashMap to store the tagInformation as key and count as value
         HashMap<String, Integer> tagInfo = new HashMap<>();
 
         // Iterate all person, and their tag to count how many occurrence.
-        model.getPersonList().stream().forEach(person -> {
+        patientList.stream().forEach(person -> {
             person.getTags().stream().forEach(tag -> {
+                // If there is a value, increment it, otherwise we start from 1.
                 tagInfo.put(tag.tagName, tagInfo.getOrDefault(tag.tagName, 0) + 1);
             });
         });
@@ -45,12 +63,8 @@ public class PatientTagViewer extends UiPart<Region> {
                 // Limit length to prevent malicious users.
                 .limit(MAX_TAGS_TO_BE_DISPLAYED).forEach(tag -> {
                     addLabel(tag.getKey() + " (" + tag.getValue() + ")");
-                }
-        );
+                });
     }
-
-
-
 
     private void addLabel(String labelText) {
         Label label = new Label(labelText);
