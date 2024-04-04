4dfdebb04d40befd9863959a66ec4e5cb13c79eb
vnnamng <e0969384@u.nus.edu>
2024-04-04 08:40:37 +0800

    Make prefix optional

--- src/main/java/seedu/address/logic/parser/FilterAppointmentCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FilterAppointmentCommandParser.java
@@ -10,6 +10,8 @@ import seedu.address.model.appointment.EndDateTime;
 import seedu.address.model.appointment.FilterAppointmentPredicate;
 import seedu.address.model.appointment.StartDateTime;
 
+import java.time.LocalDateTime;
+
 /**
  * Parses input arguments and creates a new FindAppointmentCommand object
  */
@@ -23,16 +25,25 @@ public class FilterAppointmentCommandParser implements Parser<FilterAppointmentC
     public FilterAppointmentCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_START_DATETIME, PREFIX_END_DATETIME);
 
-        if (!ParserUtil.arePrefixesPresent(argMultimap, PREFIX_START_DATETIME, PREFIX_END_DATETIME)
-                || !argMultimap.getPreamble().isEmpty()) {
+        if (!argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterAppointmentCommand.MESSAGE_USAGE));
         }
 
         argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_START_DATETIME, PREFIX_END_DATETIME);
-        StartDateTime startDateTime = ParserUtil.parseStartDateTime(argMultimap.getValue(PREFIX_START_DATETIME).get());
-        EndDateTime endDateTime = ParserUtil.parseEndDateTime(argMultimap.getValue(PREFIX_END_DATETIME).get());
-
+        StartDateTime startDateTime;
+        EndDateTime endDateTime;
+        if (argMultimap.getValue(PREFIX_START_DATETIME).isEmpty()) {
+            startDateTime = new StartDateTime(LocalDateTime.MIN);
+        } else {
+            startDateTime = ParserUtil.parseStartDateTime(argMultimap.getValue(PREFIX_START_DATETIME).get());
+        }
+        if (argMultimap.getValue(PREFIX_END_DATETIME).isEmpty()) {
+            endDateTime = new EndDateTime(LocalDateTime.MAX);
+        } else {
+            endDateTime = ParserUtil.parseEndDateTime(argMultimap.getValue(PREFIX_END_DATETIME).get());
+        }
+        
         return new FilterAppointmentCommand(new FilterAppointmentPredicate(startDateTime, endDateTime),
                 startDateTime, endDateTime);
     }
