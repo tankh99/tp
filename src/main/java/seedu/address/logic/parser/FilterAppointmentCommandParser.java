3c0cde51a809fb68f9624266f5f0db3bdd94c0b8
vnnamng <e0969384@u.nus.edu>
2024-04-04 08:43:20 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/FilterAppointmentCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FilterAppointmentCommandParser.java
@@ -4,13 +4,13 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_END_DATETIME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_START_DATETIME;
 
+import java.time.LocalDateTime;
+
 import seedu.address.logic.commands.FilterAppointmentCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.appointment.EndDateTime;
 import seedu.address.model.appointment.FilterAppointmentPredicate;
 import seedu.address.model.appointment.StartDateTime;
-
-import java.time.LocalDateTime;
 
 /**
  * Parses input arguments and creates a new FindAppointmentCommand object
@@ -20,6 +20,7 @@ public class FilterAppointmentCommandParser implements Parser<FilterAppointmentC
     /**
      * Parses the given {@code String} of arguments in the context of the FindAppointmentCommand
      * and returns a FindAppointmentCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public FilterAppointmentCommand parse(String args) throws ParseException {
@@ -43,7 +44,7 @@ public class FilterAppointmentCommandParser implements Parser<FilterAppointmentC
         } else {
             endDateTime = ParserUtil.parseEndDateTime(argMultimap.getValue(PREFIX_END_DATETIME).get());
         }
-        
+
         return new FilterAppointmentCommand(new FilterAppointmentPredicate(startDateTime, endDateTime),
                 startDateTime, endDateTime);
     }
--- src/main/java/seedu/address/model/appointment/FilterAppointmentPredicate.java
+++ src/main/java/seedu/address/model/appointment/FilterAppointmentPredicate.java
@@ -11,20 +11,21 @@ public class FilterAppointmentPredicate implements Predicate<Appointment> {
 
     /**
      * Finds appointment based on start and end date times.
+     *
      * @param lowerBoundDateTime target start date time.
      * @param upperBoundDateTime target end date time.
      */
 
     public FilterAppointmentPredicate(StartDateTime lowerBoundDateTime, EndDateTime upperBoundDateTime) {
-            this.lowerBoundDateTime = lowerBoundDateTime;
-            this.upperBoundDateTime = upperBoundDateTime;
-        }
+        this.lowerBoundDateTime = lowerBoundDateTime;
+        this.upperBoundDateTime = upperBoundDateTime;
+    }
 
-        @Override
-        public boolean test(Appointment appointment) {
-            boolean isAppointmentFullyBeforeLowerBound = appointment.getEndDateTime().isBefore(lowerBoundDateTime);
-            boolean isAppointmentFullyAfterUpperBound = appointment.getStartDateTime().isAfter(upperBoundDateTime);
-            // Filter out all eliminated cases
-            return !(isAppointmentFullyBeforeLowerBound || isAppointmentFullyAfterUpperBound);
-        }
+    @Override
+    public boolean test(Appointment appointment) {
+        boolean isAppointmentFullyBeforeLowerBound = appointment.getEndDateTime().isBefore(lowerBoundDateTime);
+        boolean isAppointmentFullyAfterUpperBound = appointment.getStartDateTime().isAfter(upperBoundDateTime);
+        // Filter out all eliminated cases
+        return !(isAppointmentFullyBeforeLowerBound || isAppointmentFullyAfterUpperBound);
+    }
 }
