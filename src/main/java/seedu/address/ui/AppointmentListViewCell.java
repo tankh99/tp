578b23ae974d0abec533979162e8706d7817375a
caitlyntang <caitlyntong6@gmail.com>
2024-04-04 13:01:54 +0800

    Fix checkstyle

--- src/main/java/seedu/address/ui/AppointmentListViewCell.java
+++ src/main/java/seedu/address/ui/AppointmentListViewCell.java
@@ -1,12 +1,8 @@
 package seedu.address.ui;
 
-import java.util.List;
-
-import javafx.collections.ObservableList;
 import javafx.scene.control.ListCell;
 import seedu.address.model.ReadOnlyPatientList;
 import seedu.address.model.appointment.Appointment;
-import seedu.address.model.patient.Patient;
 
 /**
  * Custom {@code ListCell} that displays the graphics of a {@code Appointment} using an {@code AppointmentCard}.
