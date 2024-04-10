51e7c35045cd1cfb9c354c23280b886d368234c2
Jerome-j <49815108+Jerome-j@users.noreply.github.com>
2024-04-11 05:58:28 +0800

    Resolve bug

--- src/main/java/seedu/address/model/tag/Tag.java
+++ src/main/java/seedu/address/model/tag/Tag.java
@@ -9,9 +9,8 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class Tag {
 
-    public static final String MESSAGE_CONSTRAINTS = "Tag names should be alphanumeric and should not be blank";
+    public static final String MESSAGE_CONSTRAINTS = "The values for affiliated-with should be alphanumeric and should not be blank";
     public static final String VALIDATION_REGEX = "\\p{Alnum}+";
-
     public final String tagName;
 
     /**
