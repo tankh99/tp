faf4c5c2ccde8d77e4d13f863beed6f5c6a62265
tankh99 <khanghou99@gmail.com>
2024-04-13 17:48:56 +0800

    Fix audio not working in jar

--- src/main/java/seedu/address/commons/util/AudioUtil.java
+++ src/main/java/seedu/address/commons/util/AudioUtil.java
@@ -1,23 +1,49 @@
 package seedu.address.commons.util;
 
-import java.io.File;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
+
 /**
  * Contains auto-specific code
  */
 public class AudioUtil {
 
-    public static final String ERROR_SOUND = "assets/boop.mp3";
+    public static final String ERROR_SOUND = "/assets/boop.wav";
     /**
      * Plays an audio given a file path. Note that there is a slight delay due to having to load up the media player
      *
      * @param audioPath File path to the audio file to play. Note that directory starts from project root
      */
     public static void playAudio(String audioPath) {
-        Media media = new Media(new File(audioPath).toURI().toString());
-        MediaPlayer mediaPlayer = new MediaPlayer(media);
-        mediaPlayer.play();
+        try {
+            InputStream inputStream = AudioUtil.class.getResourceAsStream(ERROR_SOUND);
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new BufferedInputStream(inputStream));
+            AudioFormat format = audioInputStream.getFormat();
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+            SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+            line.open(format);
+            line.start();
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = audioInputStream.read(buffer)) != -1) {
+                line.write(buffer, 0, bytesRead);
+            }
+
+            line.drain();
+            line.close();
+            audioInputStream.close();
+        } catch (IOException | UnsupportedAudioFileException | LineUnavailableException ex) {
+            ex.printStackTrace();
+        }
     }
 }
