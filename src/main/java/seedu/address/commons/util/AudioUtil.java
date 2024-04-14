463fa431486eaae854330c38f1793965d72121c3
tankh99 <khanghou99@gmail.com>
2024-04-14 18:57:29 +0800

    Fix audio file affecting UI
    - However, note that this makes it so that every UP arrow is not guaranteed to trigger a sound

--- src/main/java/seedu/address/commons/util/AudioUtil.java
+++ src/main/java/seedu/address/commons/util/AudioUtil.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
@@ -18,32 +19,31 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 public class AudioUtil {
 
     public static final String ERROR_SOUND = "/assets/boop.wav";
+
+    private static Clip clip;
+
+    // Load audio file at the beginning
+    static {
+        try {
+            // Load audio input stream
+            InputStream inputStream = AudioUtil.class.getResourceAsStream(ERROR_SOUND);
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new BufferedInputStream(inputStream));
+
+            clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
     /**
      * Plays an audio given a file path. Note that there is a slight delay due to having to load up the media player
      *
      * @param audioPath File path to the audio file to play. Note that directory starts from project root
      */
     public static void playAudio(String audioPath) {
-        try {
-            InputStream inputStream = AudioUtil.class.getResourceAsStream(ERROR_SOUND);
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new BufferedInputStream(inputStream));
-            AudioFormat format = audioInputStream.getFormat();
-            DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
-            SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
-            line.open(format);
-            line.start();
-
-            byte[] buffer = new byte[4096];
-            int bytesRead;
-            while ((bytesRead = audioInputStream.read(buffer)) != -1) {
-                line.write(buffer, 0, bytesRead);
-            }
-
-            line.drain();
-            line.close();
-            audioInputStream.close();
-        } catch (IOException | UnsupportedAudioFileException | LineUnavailableException ex) {
-            ex.printStackTrace();
+        if (clip != null) {
+            clip.setFramePosition(0);
+            clip.start();
         }
     }
 }
